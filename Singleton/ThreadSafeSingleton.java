bdc7e23a123320f2920146f2fd299c03ace911d1
Prakhar Gupta <mywolfdale@gmail.com>
2017-11-15 22:55:18 +0530

    Create ThreadSafeSingleton.java

---
+++ Singleton/ThreadSafeSingleton.java
@@ -0,0 +1,16 @@
+//Using Synchronize block
+
+class ThreadSafeSingleton {
+
+    private static ThreadSafeSingleton instance;
+    
+    private ThreadSafeSingleton(){}
+    
+    public static synchronized ThreadSafeSingleton getInstance(){
+        if(instance == null){
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+    
+}
