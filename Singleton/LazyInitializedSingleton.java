fb61bcb9f7ca5245a3fb4497ab91e38fd58fac7c
Prakhar Gupta <mywolfdale@gmail.com>
2017-11-15 22:54:18 +0530

    Create LazyInitializedSingleton.java

---
+++ Singleton/LazyInitializedSingleton.java
@@ -0,0 +1,13 @@
+class LazyInitializedSingleton {
+
+    private static LazyInitializedSingleton instance;
+    
+    private LazyInitializedSingleton(){}
+    
+    public static LazyInitializedSingleton getInstance(){
+        if(instance == null){
+            instance = new LazyInitializedSingleton();
+        }
+        return instance;
+    }
+}
